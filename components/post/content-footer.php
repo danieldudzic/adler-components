2c5a1881a8ba649dd7f67b613a2fa1ebb44c4ffc
Danny Dudzic <daniel.dudzic@automattic.com>
2017-03-20 03:35:26 +0100

    Switch from Codekit 2 to Codekit 3

--- components/post/content-footer.php
+++ components/post/content-footer.php
@@ -1,5 +1,13 @@
 	<footer class="entry-footer">
-		<?php if ( ! is_single() && 'post' === get_post_type() ) { get_template_part( 'components/post/content', 'meta' ); } ?>
+		<?php
+			if ( ! is_single() && 'post' === get_post_type() ) {
+				get_template_part( 'components/post/content', 'meta' );
+			}
+		?>
 		<?php adler_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
-	<?php if ( is_single() && 'post' === get_post_type() ) { adler_author_bio(); } ?>
+	<?php
+		if ( is_single() && 'post' === get_post_type() ) {
+			adler_author_bio();
+		}
+	?>
