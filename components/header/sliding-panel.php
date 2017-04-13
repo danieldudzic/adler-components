aa361db469200e3900edf7a50aaaf5f8e589f75e
Danny Dudzic <daniel.dudzic@automattic.com>
2017-04-14 01:12:32 +0200

    Fix code indentation for the sliding-panel.php file

--- components/header/sliding-panel.php
+++ components/header/sliding-panel.php
@@ -1,25 +1,25 @@
 <?php if ( is_active_sidebar( 'sidebar-1' ) || has_nav_menu( 'top' ) || has_nav_menu( 'social' ) ) : ?>
-			<button class="menu-toggle animated" aria-expanded="false" >
-				<span class="menu-toggle-icon">
-					<?php echo adler_get_svg( array(
-						'icon' => 'menu-toggle',
-					) ); ?>
-				</span>
-				<span class="screen-reader-text"><?php esc_html_e( 'Toggle Menu', 'adler' ); ?></span>
-			</button>
-			<div class="slide-panel animated closed">
-				<?php adler_social_menu(); ?>
+	<button class="menu-toggle animated" aria-expanded="false" >
+		<span class="menu-toggle-icon">
+			<?php echo adler_get_svg( array(
+				'icon' => 'menu-toggle',
+			) ); ?>
+		</span>
+		<span class="screen-reader-text"><?php esc_html_e( 'Toggle Menu', 'adler' ); ?></span>
+	</button>
+	<div class="slide-panel animated closed">
+		<?php adler_social_menu(); ?>
 
-				<?php if ( has_nav_menu( 'top' ) ) : ?>
-					<nav id="site-navigation" class="main-navigation" role="navigation">
-						<?php wp_nav_menu( array(
-							'theme_location' => 'top',
-						) ); ?>
-					</nav>
-				<?php endif; ?>
-
-				<?php if ( is_active_sidebar( 'sidebar-1' ) ) {
-					get_sidebar();
-} ?>
-			</div>
+		<?php if ( has_nav_menu( 'top' ) ) : ?>
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<?php wp_nav_menu( array(
+					'theme_location' => 'top',
+				) ); ?>
+			</nav>
 		<?php endif; ?>
+
+		<?php if ( is_active_sidebar( 'sidebar-1' ) ) {
+			get_sidebar();
+		} ?>
+	</div>
+<?php endif; ?>
