1331c3a4a5b132757f1b54fc1adea842657301ba
Danny Dudzic <daniel.dudzic@automattic.com>
2017-04-15 01:40:39 +0200

    Add original theme author credits in the footer

--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,5 +1,5 @@
 <div class="site-info">
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'adler' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'adler' ), 'WordPress' ); ?></a>
 	<span class="sep"> | </span>
-	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'adler' ), 'Adler', '<a href="https://wordpress.com/themes/" rel="designer">WordPress.com</a>' ); ?>
+	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'adler' ), 'Adler', '<a href="http://pixelgrade.com/" rel="designer">PixelGrade</a>' ); ?>
 </div><!-- .site-info -->
