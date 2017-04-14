f7296ff2580b3368757dfbfd8855a166e26f5a4c
Danny Dudzic <daniel.dudzic@automattic.com>
2017-04-14 02:17:33 +0200

    Replace Older/Newer comment links with the_comments_navigaiton(). Update the Icon Functions file comments.

--- comments.php
+++ comments.php
@@ -35,17 +35,7 @@ if ( post_password_required() ) {
 			?>
 		</h2>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'adler' ); ?></h2>
-			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'adler' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'adler' ) ); ?></div>
-
-			</div>
-		</nav>
-		<?php endif; // Check for comment navigation. ?>
+		<?php the_comments_navigation(); ?>
 
 		<ol class="comment-list">
 			<?php
@@ -56,21 +46,9 @@ if ( post_password_required() ) {
 				) );
 			?>
 		</ol>
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'adler' ); ?></h2>
-			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'adler' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'adler' ) ); ?></div>
-
-			</div>
-		</nav>
-		<?php
-		endif; // Check for comment navigation.
+		<?php the_comments_navigation();
 
 	endif; // Check for have_comments().
-
 
 	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
