1780179aac3255ea12c418836a37c555c2953086
Danny Dudzic <daniel.dudzic@automattic.com>
2017-04-14 17:41:44 +0200

    Convert the leaf icon from .png to .svg

--- components/post/content-footer.php
+++ components/post/content-footer.php
@@ -1,9 +1,17 @@
-	<footer class="entry-footer">
-		<?php if ( ! is_single() && 'post' === get_post_type() ) {
-			get_template_part( 'components/post/content', 'meta' );
+<footer class="entry-footer">
+	<?php if ( ! is_single() && 'post' === get_post_type() ) {
+		get_template_part( 'components/post/content', 'meta' );
+	} ?>
+	<?php adler_entry_footer(); ?>
+</footer><!-- .entry-footer -->
+<?php if ( is_single() && 'post' === get_post_type() ) {
+	adler_author_bio();
 } ?>
-		<?php adler_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-	<?php if ( is_single() && 'post' === get_post_type() ) {
-		adler_author_bio();
+
+<?php if ( ! is_single() && 'post' === get_post_type() ) {
+	echo '<span class="leaf">' .
+			adler_get_svg( array(
+				'icon' => 'leaf',
+			) ) .
+		 '</span>';
 } ?>
