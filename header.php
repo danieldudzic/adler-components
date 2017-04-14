58eac0ba4d808d06717f3f3fde5f2358728b9f2d
Danny Dudzic <daniel.dudzic@automattic.com>
2017-04-14 02:51:42 +0200

    Use <p> instead of <h1> on non-front pages for the Site Title and <p> instead of <h2> for the Description

--- header.php
+++ header.php
@@ -28,9 +28,20 @@
 	<?php get_template_part( 'components/header/sliding', 'panel' ); ?>
 
 			<div class="site-branding">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</div>
+				<?php
+				if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+				endif;
+
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
+				endif; ?>
+			</div><!-- .site-branding -->
 
 	<?php adler_the_custom_logo(); ?>
 		</header>
