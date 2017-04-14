f94e5d4b33a64851445c10e5fd585f5cb69ee8d5
Danny Dudzic <daniel.dudzic@automattic.com>
2017-04-15 00:11:13 +0200

    Add check to make sure no markup gets outputted when Footer menu isn't present

--- components/footer/footer-navigation.php
+++ components/footer/footer-navigation.php
@@ -1,6 +1,10 @@
-		<nav id="footer-navigation" class="footer-navigation" role="navigation">
-			<?php wp_nav_menu( array(
-				'theme_location' => 'menu-2',
-				'menu_id' => 'footer-menu',
-			) ); ?>
-		</nav>
+<?php if ( has_nav_menu( 'menu-2' ) ) : ?>
+	<nav id="footer-navigation" class="footer-navigation" role="navigation">
+	<?php
+	wp_nav_menu( array(
+		'theme_location' => 'menu-2',
+		'menu_id' => 'footer-menu',
+	) );
+	?>
+	</nav>
+<?php endif; ?>
