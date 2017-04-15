eb2921cb25e386da608f6c723325afc8a20156c8
Danny Dudzic <daniel.dudzic@automattic.com>
2017-04-15 03:26:19 +0200

    Add 'Menu' label to the menu toggle

--- components/header/sliding-panel.php
+++ components/header/sliding-panel.php
@@ -1,17 +1,23 @@
 <?php if ( is_active_sidebar( 'sidebar-1' ) || has_nav_menu( 'menu-1' ) || has_nav_menu( 'social' ) ) : ?>
+
 	<button class="menu-toggle animated" aria-expanded="false" >
+		<span class="menu-toggle-label"><?php _e( 'Menu', 'adler' ); ?></span>
+
 		<span class="menu-toggle-icon">
 			<?php echo adler_get_svg( array(
 				'icon' => 'menu-toggle',
 			) ); ?>
 		</span>
+
 		<span class="menu-toggle-icon-close">
 			<?php echo adler_get_svg( array(
 				'icon' => 'menu-toggle-close',
 			) ); ?>
 		</span>
+
 		<span class="screen-reader-text"><?php esc_html_e( 'Toggle Menu', 'adler' ); ?></span>
 	</button>
+
 	<div class="slide-panel animated closed">
 		<?php adler_social_menu(); ?>
 
@@ -27,4 +33,5 @@
 			get_sidebar();
 		} ?>
 	</div>
+
 <?php endif; ?>
