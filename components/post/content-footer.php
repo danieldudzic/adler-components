4566c44d13902b31f531ace956d9407b81dfab52
Danny Dudzic <daniel.dudzic@automattic.com>
2017-04-24 00:39:39 +0200

    Make sure the .entry-footer markup only gets outputted on the correct pages. Move margin-bottom from <p> to .entry-content.

--- components/post/content-footer.php
+++ components/post/content-footer.php
@@ -1,13 +1,13 @@
-<footer class="entry-footer">
-	<?php if ( ! is_single() && 'post' === get_post_type() ) : ?>
+<?php if ( ! is_single() && 'post' === get_post_type() ) : ?>
 
+	<footer class="entry-footer">
 		<div class="entry-meta">
 			<?php adler_entry_meta(); ?>
 			<?php adler_entry_footer(); ?>
 		</div><!-- .entry-meta -->
+	</footer><!-- .entry-footer -->
 
-	<?php endif; ?>
-</footer><!-- .entry-footer -->
+<?php endif; ?>
 
 <?php if ( is_single() && 'post' === get_post_type() ) {
 	adler_author_bio();
